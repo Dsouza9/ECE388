ed58c74236c6146fbecadf56604c473c5ed7c80c
apimentel1 <47004922+apimentel1@users.noreply.github.com>
2019-09-16 21:41:47 -0400

    Working button and led

--- main.c
+++ main.c
@@ -10,53 +10,40 @@ void mydelay(int x);
 #include <avr/io.h>
 #define F_CPU 16000000UL
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
 
-int one_hz = 50;
-int five_hz = 10;
-int ten_hz = 5;
+volatile int one_hz = 50;
+volatile int five_hz = 10;
+volatile int ten_hz = 5;
 int Delay_Time;
-int Delay_Select;
+volatile int Delay_Select = 3;
+//int Flag = 0;
+int On = 0;
 
 #define Led 5
 #define button 7
+
+uint8_t portbhistory = 0xFF; 
+
 
 
 int main(void)
 {
 	DDRB = 1<<Led; //set to input
-	PORTB |= 1<<Led; //turn led off
+	PORTB |= (1<<button); //enable pull-up
+	
+	PORTB &= ~(1<<Led); //turn led off
+	
+	PCICR |= (1 << PCIE0);
+	PCMSK0 |= (1 << PCINT7);
+	sei();
 	
 	Delay_Time = five_hz;
-	Delay_Select = 3;
 	
-    /* Replace with your application code */
+
     while (1) 
-    {
-		if((PINB & 0x80) == 0) //when the button goes low
-		{
-			while((PINB & (1<<button)) == 0)
-			{
-				//what till goes high
-			}
-			
-			if(Delay_Select == 1)
-			{
-				Delay_Time = one_hz;
-				Delay_Select = 2;
-			}
-			else if (Delay_Select == 2)
-			{
-				Delay_Time = five_hz;
-				Delay_Select = 3;
-			}
-			else if(Delay_Select == 3)
-			{
-				Delay_Time = ten_hz;
-				Delay_Select = 1;
-			}	
-		}
-		
+    {		
 		PORTB &= ~(1<< Led); //Turn led off
 		mydelay(Delay_Time);
 		PORTB |= 1 << Led; //turn led on
@@ -72,4 +59,27 @@ void mydelay(int x)
 	}
 	
 }
+
+ISR(PCINT0_vect)
+{
+	if(PINB != (PINB & ~(1<<button))) //button goes high
+	{		
+		if(Delay_Select == 1)
+		{
+			Delay_Time = one_hz;
+			Delay_Select = 2;
+		}
+		else if (Delay_Select == 2)
+		{
+			Delay_Time = five_hz;
+			Delay_Select = 3;
+		}
+		else if(Delay_Select == 3)
+		{
+			Delay_Time = ten_hz;
+			Delay_Select = 1;
+		}
+	}
+}
+
 
